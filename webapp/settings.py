2685a3fd8e67ab220727a0b4d66fa6ad557d2f74
Khanh <khanhpham.2004.02@gmail.com>
2022-12-29 16:47:15 +0700

    deloy

--- webapp/settings.py
+++ webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_66y2r1(251*mj(eo*w*q-kn7ri1g-me!-n*fa=@%$j+3@nfz^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 
@@ -127,6 +127,7 @@ DATABASES = {
 'PORT': '5432',
 }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -165,9 +166,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'webapp/static')
 ]
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
